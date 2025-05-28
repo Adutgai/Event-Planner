8cb552f411a424b726e722e6c11bccdced8999e1
adutgai <ug2321358@ines.ac.rw>
2025-05-28 16:30:14 +0200

    added index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import eventRoutes from './routes/eventRoutes.js';
+import taskRoutes from './routes/tasksRoutes.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://adut2025:data2025@cluster0.0hyugti.mongodb.net/db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log("MongoDB connected"));
+
+app.use('/api/events', eventRoutes);
+app.use('/api/tasks', taskRoutes);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
