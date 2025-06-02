e0df13a3e81806e26ada534dd20eb8252c66ee5c
adutgai <ug2321358@ines.ac.rw>
2025-06-02 09:44:52 +0200

    Added Put and Delete Routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -1,9 +1,19 @@
 import express from 'express';
-import { getAllEvents, createEvent } from '../controllers/eventController.js';
+import {
+  getAllEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} from '../controllers/eventController.js';
 
 const router = express.Router();
 
+//Get and post routes
 router.get('/', getAllEvents);
 router.post('/', createEvent);
+
+// Put and Delete Routes
+router.put('/:id', updateEvent);
+router.delete('/:id', deleteEvent);
 
 export default router;
